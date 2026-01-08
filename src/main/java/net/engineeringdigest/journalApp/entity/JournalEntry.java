7c0422cda7efe8321946225ad75b2680d9ca1115
namankaushik <kaushiknaman68@gmail.com>
2026-01-08 19:15:27 +0530

    added @NonNull in title

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -2,6 +2,7 @@ package net.engineeringdigest.journalApp.entity;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 public class JournalEntry {
     @Id
     private ObjectId id;
-     @Getter
+    @NonNull
      private String title;
      private String content;
      private LocalDateTime date;
