e0d63171ea7349a1cc09333d8e99fdb6ed2af200
namankaushik <kaushiknaman68@gmail.com>
2026-01-11 12:39:08 +0530

    Added SpringSecurity

--- src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -3,12 +3,18 @@ package net.engineeringdigest.journalApp.config;
 import net.engineeringdigest.journalApp.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -17,10 +23,11 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/journal/**").authenticated()
+                .antMatchers("/journal/**","/user/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
     }
 
     @Override
