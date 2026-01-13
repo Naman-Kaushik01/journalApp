f8fd3b6c66897b99e3b3fdd0e3a7fe4c780f629d
namankaushik <kaushiknaman68@gmail.com>
2026-01-13 11:40:49 +0530

    Testing UserService

---
+++ src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
@@ -0,0 +1,29 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Test
+    public void testFindByUserName() {
+        assertNotNull(userRepository.findByUserName("ram"));
+    }
+    @Test
+    public void testJournalEntryPresent(){
+        User user = userRepository.findByUserName("ram");
+        assertTrue(!user.getJournalEntries().isEmpty());
+    }
+
+
+}
