8c40b394589397d132bda89707ab0d3f405ef3cc
Naman Kaushik <kaushiknaman68@gmail.com>
2026-01-23 21:20:41 +0530

    sending mail through javaMailSender

--- src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
@@ -1,7 +1,27 @@
 package net.engineeringdigest.journalApp.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        try{
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(text);
+            mailSender.send(mail);
+        }catch(Exception e){
+            log.error("Exception while sending email", e);
+        }
+    }
 }
